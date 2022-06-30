8e2e4b9862622dcaaa14520d75dd88c29e3f1f45
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 15:27:58 +0430

    add `graph` ds

---
+++ Data_Structures/Directed_Graph.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+class DirectedGraph:
+    def __init__(self, vertices_count):
+        self._graph = defaultdict(list)
+        self._vertices_count = vertices_count
+
+    def add_edge(self, u, v):
+        self._graph[u].append(v)
+
+    def is_cyclic_util(self, v, visited, recursion_stack):
+
+        visited[v] = True
+        recursion_stack[v] = True
+        for neighbour in self._graph[v]:
+            if not visited[neighbour]:
+                if self.is_cyclic_util(neighbour, visited, recursion_stack):
+                    return True
+            elif recursion_stack[neighbour]:
+                return True
+        recursion_stack[v] = False
+        return False
+
+    def is_cyclic(self):
+        visited = [False] * (self._vertices_count + 1)
+        recursion_stack = [False] * (self._vertices_count + 1)
+        for node in range(self._vertices_count):
+            if not visited[node]:
+                if self.is_cyclic_util(node, visited, recursion_stack):
+                    return True
+        return False
+
+# complexity: O(V+E)
+
+
