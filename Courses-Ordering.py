b5844c6fd5774d33c2a6ee145ef030842140fb16
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 12:10:10 +0430

    create course ordering class and its `graph`

--- Courses-Ordering.py
+++ Courses-Ordering.py
@@ -1,1 +1,19 @@
+import numpy as np
+from numpy import ndarray
+from Graph.Directed_Graph import DirectedGraph
+
+class CourseOrdering:
+    _course_graph = None
+    _is_possible_to_finished = None
+
+    def __init__(self, numCourses: int, prerequisites: list[list[int]] | ndarray):
+        self.prerequisites = prerequisites
+        self.numCourses = numCourses
+
+        self._create_graph()
+
+    def _create_graph(self):
+        self._course_graph = DirectedGraph(self.numCourses)
+        for pre_req in self.prerequisites:
+            self._course_graph.add_edge(pre_req[0], pre_req[1])
 
