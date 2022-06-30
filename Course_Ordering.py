ca2b7e40a70fa3870ced9a67356e615e97ebe57e
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 12:39:49 +0430

    Update Course_Ordering.py

--- Course_Ordering.py
+++ Course_Ordering.py
@@ -1,15 +1,12 @@
-import numpy as np
-from numpy import ndarray
 from Graph.Directed_Graph import DirectedGraph
 
 class CourseOrdering:
     _course_graph = None
     _is_possible_to_finished = None
 
-    def __init__(self, numCourses: int, prerequisites: list[list[int]] | ndarray):
+    def __init__(self, numCourses, prerequisites):
         self.prerequisites = prerequisites
         self.numCourses = numCourses
-
         self._create_graph()
 
     def _create_graph(self):
@@ -18,5 +15,7 @@ class CourseOrdering:
             self._course_graph.add_edge(pre_req[0], pre_req[1])
 
     def can_courses_be_finished(self):
-        return not self._course_graph.is_cyclic()
+        if self._is_possible_to_finished is None:
+            self._is_possible_to_finished = not self._course_graph.is_cyclic()
+        return self._is_possible_to_finished
 
