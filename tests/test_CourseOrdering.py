a127f42e023d3e5c419e38039d4d388116ea97e7
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 12:40:26 +0430

    create test for `can_courses_be_finished()`

--- tests/test_CourseOrdering.py
+++ tests/test_CourseOrdering.py
@@ -1,1 +1,12 @@
+from Course_Ordering import CourseOrdering
 
+def test_CourseOrdering():
+    numCourses = 2
+    prerequisites = [[1, 0], [0, 2]]
+
+    course_ordering = CourseOrdering(numCourses, prerequisites)
+
+    print('can courses be finished?:', course_ordering.can_courses_be_finished())
+
+if __name__ == '__main__':
+    test_CourseOrdering()
