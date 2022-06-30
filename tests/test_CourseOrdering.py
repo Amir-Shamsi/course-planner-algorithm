aa69e6a864cfeee7e5a3e3d5a3a87a39dea7d721
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 15:26:26 +0430

    Update test_CourseOrdering.py

--- tests/test_CourseOrdering.py
+++ tests/test_CourseOrdering.py
@@ -1,12 +1,14 @@
 from Course_Ordering import CourseOrdering
 
 def test_CourseOrdering():
-    numCourses = 2
-    prerequisites = [[1, 0], [0, 2]]
+    numCourses = 7
+    prerequisites = [[5, 4], [1, 0], [2, 1], [2, 3], [2, 0], [1, 6], [6, 0]]
 
     course_ordering = CourseOrdering(numCourses, prerequisites)
 
     print('can courses be finished?:', course_ordering.can_courses_be_finished())
+
+    print('ordering:', course_ordering.ordering())
 
 if __name__ == '__main__':
     test_CourseOrdering()
