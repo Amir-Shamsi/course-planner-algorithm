a5c8e2af1b5acfb8780f3aef91901bfba7564e4a
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 20:45:21 +0430

    add test case #4 for `study_plan`

--- tests/test_CourseOrdering.py
+++ tests/test_CourseOrdering.py
@@ -18,6 +18,7 @@ def test_CourseOrdering_1():  # phase 4
     # prerequisites = [[1, 0], [3, 2], [5, 2], [2, 1]]
     # prerequisites = [[1, 0], [3, 2], [5, 2], [2, 0]]
     prerequisites = [[1, 0], [3, 2], [5, 2]]
+    # prerequisites = [[1, 0], [2, 0], [3, 2], [1, 3], [5, 2]]
 
     course_ordering = CourseOrdering(numCourses, prerequisites)
 
