6390da975119faf78555776ae077892fbf69b4e2
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-06-30 20:25:49 +0430

    add `Study Plan` function calculator

--- Course_Ordering.py
+++ Course_Ordering.py
@@ -1,9 +1,14 @@
+import itertools
+import math
+
 from data_structures.Directed_Graph import DirectedGraph
+
 
 class CourseOrdering(DirectedGraph):
     _course_graph = None
     _is_possible_to_finished = None
     _courses_ordering = None
+    pre_req_courses = {0: [], 1: []}
 
     def __init__(self, numCourses, prerequisites):
         super().__init__(numCourses)
@@ -12,11 +17,15 @@ class CourseOrdering(DirectedGraph):
         self._create_graph()
 
     def _create_graph(self):
-        self._all_vertices = list()
+        _remain_courses = [x for x in range(self.numCourses)]
         for pre_req in self.prerequisites:
             self.add_edge(pre_req[1], pre_req[0])
-            self._all_vertices.append(pre_req[1])
-            self._all_vertices.append(pre_req[0])
+
+            if pre_req[0] in _remain_courses: _remain_courses.remove(pre_req[0])
+            if pre_req[1] in _remain_courses: _remain_courses.remove(pre_req[1])
+
+        for course in _remain_courses:
+            self.add_edge(-1, course)
 
     def can_courses_be_finished(self):
         if self._is_possible_to_finished is None:
@@ -43,6 +52,37 @@ class CourseOrdering(DirectedGraph):
 
         self._courses_ordering = ordered
         return ordered
+
+    def study_plan(self, free_sems, addition_cost, max_courses):
+        free_courses_flag = True
+        graph = self._graph.copy()
+        graph.pop(-1)
+        courses_pick_count, total_cost, semester_count = 0, 0, 0
+        semesters, next_sem, visited_courses = [[]], [], []
+
+        while len(graph) != 0:
+            for course in sorted(graph, key=lambda course: len(graph[course]), reverse=True):
+                if course in next_sem or course in list(itertools.chain.from_iterable(graph.values())):
+                    continue
+                if courses_pick_count < max_courses:
+                    semesters[semester_count].append(course)
+                    next_sem.extend(graph.get(course))
+                    visited_courses.append(course)
+                    courses_pick_count+=1
+                    if not free_courses_flag:
+                        total_cost += addition_cost
+                else:
+                    break
+            semester_count+=1
+            semesters.append([])
+            if len(next_sem) > 0: next_sem.clear()
+            courses_pick_count = 0
+            free_courses_flag = (semester_count < free_sems)
+            for v in visited_courses:
+                graph.pop(v)
+            visited_courses.clear()
+        semesters.pop()
+        return semesters, total_cost
 
     def query(self, queries):
         return [(self._courses_ordering.index(query[1]) < self._courses_ordering.index(query[0])) for query in queries]
