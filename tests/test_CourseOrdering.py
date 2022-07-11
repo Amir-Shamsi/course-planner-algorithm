ff44e9b8f99972e77ad14aa3d7e9decea64b0f9f
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-07-11 18:20:33 +0430

    adding more complex test case

--- tests/test_CourseOrdering.py
+++ tests/test_CourseOrdering.py
@@ -14,20 +14,24 @@ def test_CourseOrdering():  # phase 1 to 3
     print('for queries `{}`: {}'.format(queries, course_ordering.query(queries)))
 
 def test_CourseOrdering_1():  # phase 4
-    numCourses = 8
+    numCourses = 13
     # prerequisites = [[1, 0], [3, 2], [5, 2], [2, 1]]
     # prerequisites = [[1, 0], [3, 2], [5, 2], [2, 0]]
-    prerequisites = [[1, 0], [3, 2], [5, 2]]
+    # prerequisites = [[1, 0], [3, 2], [5, 2]]
+
     # prerequisites = [[1, 0], [2, 0], [3, 2], [1, 3], [5, 2]]
+    prerequisites = [[7, 11], [3, 1], [6, 2], [7, 5], [7, 10], [3, 8], [6, 8], [3, 11], [3, 10], [3, 5], [3, 0], [6, 9],
+                     [3, 9], [6, 0], [7, 0], [6, 1], [3, 7], [7, 9], [6, 11], [3, 4], [3, 2], [6, 10], [3, 6], [3, 12],
+                     [7, 8], [7, 12]]
 
     course_ordering = CourseOrdering(numCourses, prerequisites)
 
     print('can courses be finished?:', course_ordering.can_be_taken())
 
     print('ordering:', course_ordering.ordering())
-    semesters, total_cost = course_ordering.study_plan(2, 100, 4)
+    semesters, total_cost = course_ordering.study_plan(1, 100, 9)
     for index in range(len(semesters)):
-        print('semesters #{}: {}'.format(index, semesters[index]))
+        print('semesters #{}: {}'.format(index + 1, semesters[index]))
 
     print('Cost of this plan: {}$'.format(total_cost))
 
