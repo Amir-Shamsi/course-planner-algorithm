79abe9ebabfa86039e95f38162cebb33fbe254a8
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-07-11 18:22:28 +0430

    creating `DFS max depth` calculator

--- data_structures/Directed_Graph.py
+++ data_structures/Directed_Graph.py
@@ -30,5 +30,20 @@ class DirectedGraph:
                     return True
         return False
 
+    def get_dfs_max_lens(self, v, visited, dfs_len_stack):
 
+        visited[v] = True
+        for neighbour in self._graph[v]:
+            # if not visited[neighbour]:
+            dfs_len_stack[v][0] = max(dfs_len_stack[v][0], self.get_dfs_max_lens(neighbour, visited, dfs_len_stack)+1)
+            return dfs_len_stack[v][0]
+        return 1
+
+    def dfs_max_lens(self):
+        visited = [False] * self._vertices_count
+        dfs_len_stack = {node: [0, len(self._graph[node])] for node in self._graph}
+        for node in range(self._vertices_count):
+            if not visited[node] and len(self._graph[node]) > 0:
+                self.get_dfs_max_lens(node, visited, dfs_len_stack)
+        return dfs_len_stack
 
